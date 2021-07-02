2ded873d1acba287bea35c6dc583916bb0d571ca
anamihiczunic <anamihiczunic@gmail.com>
2021-07-02 15:14:19 +0200

    Added bootstrap ecommerce template

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,7 +1,3 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
 
 const images = require.context('../img', true)
 
@@ -14,8 +10,13 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
+// import 'slick-carousel/slick/slick'
 import '../js/bootstrap_js_files.js'
+import "@fortawesome/fontawesome-free/js/all"
+import '../js/templatemo.js'
+
 import magicAdd from '../js/magicAdd.js'
+
 
 let a = magicAdd(2, 4);
 
